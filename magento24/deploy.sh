6ffe6402475439981b84c5fa91a01e9a8e2f4bd3
Firas220890 <66754928+Firas220890@users.noreply.github.com>
2023-01-17 14:04:32 +0400

    Delete deploy.sh

--- magento24/deploy.sh
+++
@@ -1,7 +0,0 @@
-php bin/magento maintenance:enable;
-php bin/magento setup:upgrade;
-php bin/magento setup:di:compile;
-php bin/magento setup:static-content:deploy -f;
-php bin/magento cache:flush;
-php bin/magento cache:clean;
-php bin/magento maintenance:disable;
